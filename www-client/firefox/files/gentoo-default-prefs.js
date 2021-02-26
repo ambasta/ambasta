ed06687a9c094c5f76ee3d77dd3f4dce845c0973
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2021-02-26 13:38:53 +0530

    gnome-base/gsettings-desktop-schemas: New ebuild, cherry pick from gnome 40 to support xsettings for fonts

--- www-client/firefox/files/gentoo-default-prefs.js
+++ www-client/firefox/files/gentoo-default-prefs.js
@@ -1,12 +1,12 @@
 pref("general.smoothScroll",               true);
 pref("general.autoScroll",                 false);
 pref("browser.urlbar.hideGoButton",        true);
-pref("accessibility.typeaheadfind",        false);
+pref("accessibility.typeaheadfind",        true);
 pref("browser.shell.checkDefaultBrowser",  false);
 pref("browser.EULA.override",              true);
 pref("general.useragent.locale",           "chrome://global/locale/intl.properties");
 pref("intl.locale.requested",              "");
 /* Disable DoH by default */
-pref("network.trr.mode",                   3);
+pref("network.trr.mode",                   5);
 /* Disable use of Mozilla Normandy service by default */
 pref("app.normandy.enabled",               false);
