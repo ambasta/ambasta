3b629299949c5eb2c3609ccd5868a257e31fed9c
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2021-06-19 13:33:34 +0530

    www-client/firefox-bin: New ebuild, no longer has hard dependency on cairo[X] since ff now uses dlsym for gtk_get_display_type

---
+++ www-client/firefox-bin/files/gentoo-default-prefs.js
@@ -0,0 +1,10 @@
+pref("general.smoothScroll",               true);
+pref("general.autoScroll",                 false);
+pref("browser.urlbar.hideGoButton",        true);
+pref("accessibility.typeaheadfind",        true);
+pref("browser.shell.checkDefaultBrowser",  false);
+pref("browser.EULA.override",              true);
+pref("general.useragent.locale",           "chrome://global/locale/intl.properties");
+pref("intl.locale.requested",              "");
+/* Disable use of Mozilla Normandy service by default */
+pref("app.normandy.enabled",               false);
