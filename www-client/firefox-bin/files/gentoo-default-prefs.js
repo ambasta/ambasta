024f3263c2f55c4f190df2fb31b3432ea6cdc95a
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2021-08-11 11:18:45 +0530

    www-client/firefox-bin: Added missing FILESDIR/files

--- www-client/firefox-bin/files/gentoo-default-prefs.js
+++ www-client/firefox-bin/files/gentoo-default-prefs.js
@@ -7,6 +7,6 @@ pref("browser.EULA.override",              true);
 pref("general.useragent.locale",           "chrome://global/locale/intl.properties");
 pref("intl.locale.requested",              "");
 /* Disable DoH by default */
-pref("network.trr.mode",                   3);
+pref("network.trr.mode",                   5);
 /* Disable use of Mozilla Normandy service by default */
 pref("app.normandy.enabled",               false);
